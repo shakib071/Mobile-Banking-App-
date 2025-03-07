b89cc0c40d1e1ab9962b7b948818861388a8a886
shakib071 <shakibhasan071@gmail.com>
2025-03-08 03:53:56 +0600

    trying qr code

--- js/send_money.js
+++ js/send_money.js
@@ -10,10 +10,29 @@ document.getElementById('send-money-back-btn').addEventListener('click',
 
 //qr code 
 
+function onScanSuccess(decodedText, decodedResult) {
+    //     decodedText	The actual content of the scanned QR code (e.g., a URL, text, etc.).
+    // decodedResult	Additional QR code details (e.g., raw scan data, format, etc.).
+    
+    document.getElementById('qr-result').innerText = "Scanned QR: " + decodedText;
+}
+
+function startQRScanner() {
+    
+    let qrScanner = new Html5Qrcode("qr-reader");
+    qrScanner.start(
+        { facingMode: "environment" },
+        { fps: 10, qrbox: 250 }, 
+        onScanSuccess
+    ).catch(err => {
+        alert("error scanning QR code");
+    });
+   
+}
 
 document.getElementById('send-money-scan-qr').addEventListener('click',
     function () {
-        console.log("hello");
+        startQRScanner();
     }
 );
 
