6edc1aa4d7e1fc412dd17a64f02d25846ce41c01
shakib071 <shakibhasan071@gmail.com>
2025-03-08 03:32:58 +0600

    trying qr code

--- js/send_money.js
+++ js/send_money.js
@@ -1,7 +1,36 @@
 
+//back button
 
 document.getElementById('send-money-back-btn').addEventListener('click',
     function () {
         window.location.href = '../home.html'
     }
 );
+
+
+//qr code 
+
+
+document.getElementById('send-money-scan-qr').addEventListener('click',
+    function () {
+        console.log("hello");
+    }
+);
+
+
+
+//next btn
+
+document.getElementById('send-money-next-btn').addEventListener('click',
+    function (){
+        let accNum = document.getElementById('send-money-acc-num').value;
+        if(accNum){
+            console.log(accNum);
+        }
+        else{
+            console.log('painai');
+        }
+        
+    }
+);
+
