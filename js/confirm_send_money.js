21fff1989e94a07e58e38ea860708b3c47f9cc06
shakib071 <shakibhasan071@gmail.com>
2025-03-09 03:38:45 +0600

    added animation for touch

--- js/confirm_send_money.js
+++ js/confirm_send_money.js
@@ -9,6 +9,7 @@ const fillLayer = document.getElementById("fillLayer");
 let holdTimeout;
 
 // Animation end listener
+
 fillLayer.addEventListener('animationend', (event) => {
     if (event.animationName === 'fillAnimation') {
         console.log("Hello World");
@@ -16,6 +17,7 @@ fillLayer.addEventListener('animationend', (event) => {
     }
 });
 
+//for mouce 
 // Start the animation after a certain hold time (e.g., 200ms)
 outerCircle.addEventListener("mousedown", () => {
     holdTimeout = setTimeout(() => {
@@ -34,6 +36,29 @@ outerCircle.addEventListener("mouseleave", () => {
     clearTimeout(holdTimeout);  // Cancel the timeout if mouse leaves
     fillLayer.classList.remove("animate-fill");  // Reset animation
 });
+
+
+// for touch
+
+outerCircle.addEventListener('touchstart', () =>
+{
+    holdTimeout = setTimeout(() => {
+        fillLayer.classList.add("animate-fill");
+    }, 200); 
+});
+
+
+outerCircle.addEventListener("touchend", () => {
+    clearTimeout(holdTimeout);  // Cancel the timeout if released early
+    fillLayer.classList.remove("animate-fill");  // Reset animation
+});
+
+
+outerCircle.addEventListener("touchcancel", () => {
+    clearTimeout(holdTimeout);  // Cancel the timeout if mouse leaves
+    fillLayer.classList.remove("animate-fill");  // Reset animation
+});
+
 
 
 
