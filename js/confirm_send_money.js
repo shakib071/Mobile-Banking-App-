ccfa9eadcc26b07ffd244faae1cf971a23dfe977
shakib071 <shakibhasan071@gmail.com>
2025-03-09 02:06:53 +0600

    adde confirm money design with animation and after animation console

--- js/confirm_send_money.js
+++ js/confirm_send_money.js
@@ -4,13 +4,23 @@
 
 const outerCircle = document.getElementById("outerCircle");
 const fillLayer = document.getElementById("fillLayer");
+
+
 let holdTimeout;
+
+// Animation end listener
+fillLayer.addEventListener('animationend', (event) => {
+    if (event.animationName === 'fillAnimation') {
+        console.log("Hello World");
+        
+    }
+});
 
 // Start the animation after a certain hold time (e.g., 200ms)
 outerCircle.addEventListener("mousedown", () => {
     holdTimeout = setTimeout(() => {
         fillLayer.classList.add("animate-fill");
-    }, 200);  // Delay before animation starts (500ms)
+    }, 200);  // Delay before animation starts (200ms)
 });
 
 // Reset if the mouse is released early
@@ -24,3 +34,7 @@ outerCircle.addEventListener("mouseleave", () => {
     clearTimeout(holdTimeout);  // Cancel the timeout if mouse leaves
     fillLayer.classList.remove("animate-fill");  // Reset animation
 });
+
+
+
+
